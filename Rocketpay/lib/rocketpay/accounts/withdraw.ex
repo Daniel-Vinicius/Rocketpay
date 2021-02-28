31e3ddbf0c1f6b4157cd400f929a7943d4b7bbfd
Daniel Vinícius <dev.dan.programador@gmail.com>
2021-02-27 22:39:10 -0300

    Aula 4

--- Rocketpay/lib/rocketpay/accounts/withdraw.ex
+++ Rocketpay/lib/rocketpay/accounts/withdraw.ex
@@ -1,17 +1,17 @@
-defmodule Rocketpay.Accounts.Deposit do
+defmodule Rocketpay.Accounts.Withdraw do
   alias Rocketpay.Repo
   alias Rocketpay.Accounts.Operation
 
   def call(params) do
     params
-    |> Operation.call(:deposit)
+    |> Operation.call(:withdraw)
     |> run_transaction()
   end
 
   defp run_transaction(multi) do
     case Repo.transaction(multi) do
       {:error, _operation, reason, _changes} -> {:error, reason}
-      {:ok, %{account_deposit: account}} -> {:ok, account}
+      {:ok, %{account_withdraw: account}} -> {:ok, account}
     end
   end
 end
--- Rocketpay/lib/rocketpay_web/router.ex
+++ Rocketpay/lib/rocketpay_web/router.ex
@@ -14,6 +14,8 @@ defmodule RocketpayWeb.Router do
 
     post "/accounts/:id/deposit", AccountsController, :deposit
     post "/accounts/:id/withdraw", AccountsController, :withdraw
+
+    post "/accounts/transaction", AccountsController, :transaction
   end
 
   # Enables LiveDashboard only for development
